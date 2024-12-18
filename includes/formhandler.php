6f2cbe7c6fd9991f255c1d0e95729a4d06727663
ChenDario <chen.dario@itismeucc.com>
2024-12-18 10:45:32 +0100

    first commit

---
+++ includes/formhandler.php
@@ -0,0 +1,65 @@
+<!-- risultato.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/formhandler.css">
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title> Answer </title>
+</head>
+<body>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+            $name = htmlspecialchars($_POST["name"]);
+            $age = htmlspecialchars($_POST["age"]);
+            $materia = htmlspecialchars($_POST["materia"]);
+        }
+    ?> 
+
+    <div class="table-ans">
+        <table>
+        <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+        <tr>
+            <td>Ernst Handel</td>
+            <td>Roland Mendel</td>
+            <td>Austria</td>
+        </tr>
+        <tr>
+            <td>Island Trading</td>
+            <td>Helen Bennett</td>
+            <td>UK</td>
+        </tr>
+        <tr>
+            <td>Laughing Bacchus Winecellars</td>
+            <td>Yoshi Tannamuri</td>
+            <td>Canada</td>
+        </tr>
+        <tr>
+            <td>Magazzini Alimentari Riuniti</td>
+            <td>Giovanni Rovelli</td>
+            <td>Italy</td>
+        </tr>
+        </table>
+    </div>   
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title> PHP Es3 </title>
+</head>
+<body>
+    <div class="form">
+        <form action="includes/formhandler.php" method="POST">
+            <div class="mb-3">
+                <label for="name" class="form-label"> Name </label>
+                <input type="text" class="form-control form-control-lg" id="name" placeholder="Enter your name..." name="name">
+            </div>
+            <div class="mb-3">
+                <label for="age" class="form-label"> Age </label>
+                <input type="number " class="form-control form-control-lg" id="age" placeholder="Enter your age..." name="age" min=0 max=100>
+            </div>
+
+            <div class="info">
+                <table class="table table-borderless">
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Frequenza di Pagamento</th>
+                            <th scope="col">Sconto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mensile</td>
+                            <td>None</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Bimestrale</td>
+                            <td>10%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Trimestrale</td>
+                            <td>15%</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Annuale</td>
+                            <td>20%</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div>
+                <h3> Inserire il piano di pagamento </h3>
+                <br>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="materia" id="informatica" value="informatica">
+                    <label class="form-check-label" for="informatica">
+                        1
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="materia" id="flexRadioDefault2" value="sistemi" checked>
+                    <label class="form-check-label" for="sistemi">
+                        2
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="materia" id="tpsit" value="tpsit" checked>
+                    <label class="form-check-label" for="tpsit">
+                        3
+                    </label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="materia" id="sis" value="sis" checked>
+                    <label class="form-check-label" for="sis">
+                        4
+                    </label>
+                </div>
+            </div>
+            
+            <div class="div-btn p-5">
+                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
